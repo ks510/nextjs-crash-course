b8bf4b877901dcf3efdb88bfa744e01ef78bb5a0
Karen <ksuen.dev@gmail.com>
2022-04-26 00:57:14 +0100

    feat(header): add styling and content to Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,15 @@
 import { FC } from 'react';
+import headerStyles from '../styles/header.module.scss';
 
 const Header: FC = () => {
   return (
-    <div></div>
+    <div>
+      <h1 className={headerStyles.title}>
+        <span>WebDev</span> News
+      </h1>
+      <p className={headerStyles.description}>Keep up to date with the latest web dev news</p>
+    </div>
   );
 };
+
+export default Header;
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import styles from '../styles/Layout.module.scss';
 import React, { FC } from 'react';
 import Nav from './Nav';
+import Header from './Header';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -8,12 +9,14 @@ interface LayoutProps {
 
 const Layout: FC<LayoutProps> = (props) => {
   const { children } = props;
+  const x = true;
 
   return (
     <>
       <Nav />
       <div className={styles.container}>
         <main className={styles.main}>
+          <Header />
           {children}
         </main>
       </div>
