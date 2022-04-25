b15da4e783d1361a40fcc20154e8949cbc79e4aa
Karen <ksuen.dev@gmail.com>
2022-04-25 21:36:51 +0100

    feat(app): add metadata to pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,10 @@ import Head from 'next/head';
 const Home: NextPage = () => {
   return (
     <div>
+      <Head>
+        <title>WebDev Newz</title>
+        <meta name="keywords" content="web development, programming" />
+      </Head>
       <h1>Welcome to Next</h1>
     </div>
   );
