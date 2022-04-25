0d0da51ff54381008425bd3a4c770389a30c7203
Karen <ksuen.dev@gmail.com>
2022-04-25 22:48:12 +0100

    feat(app): create header component and styles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import styles from '../styles/Layout.module.scss';
 import React, { FC } from 'react';
+import Nav from './Nav';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -9,12 +10,16 @@ const Layout: FC<LayoutProps> = (props) => {
   const { children } = props;
 
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        {children}
-      </main>
-    </div>
-  );
+    <>
+      <Nav />
+      <div className={styles.container}>
+        <main className={styles.main}>
+          {children}
+        </main>
+      </div>
+    </>
+  )
+    ;
 };
 
 export default Layout;
